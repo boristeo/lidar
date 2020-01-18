39dd56d130ea60d76e769927d330c7659567dc32
Boris Teodorovich <boristeodorovich@gmail.com>
2020-01-18 00:17:40 -0800

    Refactoring, improvements, and an attempt at the positioning problem

--- lidar.py
+++ lidar.py
@@ -1,8 +1,3 @@
-#Display Data from Neato LIDAR
-#based on code from Nicolas "Xevel" Saugnier
-#requires vpython and pyserial
-
-
 import thread
 import time
 import sys
@@ -12,66 +7,77 @@ import struct
 import serial
 from visual import *
 
-com_port = "COM5" # example: 5 == "COM6" == "/dev/tty5"
-baudrate = 250000
-visualization = True
-
-lidarData = [[] for i in range(360)] #A list of 360 elements Angle, Distance , quality
-lidar_rpm = 0
+sincos = [(math.sin(math.radians(a)), math.cos(math.radians(a))) for a in range(360)]
+lidar_data = [(0, 0) for i in range(360)]
 
 ZERO_I_OFFSET = 140
 
 
 class Visualization:
   point = points(pos=[(0,0,0) for i in range(360)], size=5, color=(0 , 1, 0))
-  pointb = points(pos=[(0,0,0) for i in range(360)], size=5, color=(0, 0.4, 0))
-  pointbb = points(pos=[(0,0,0) for i in range(360)], size=5, color=(1, 0, 0))
   pointi = points(pos=[(0,0,0) for i in range(360)], size=3, color=(0 , 1, 0))
-  pointib = points(pos=[(0,0,0) for i in range(360)], size=3, color=(1, 1, 0))
-  pointibb = points(pos=[(0,0,0) for i in range(360)], size=3, color=(0.4, 0, 0))
-
-  zero_intensity_ring = ring(pos=(0,0,0), axis=(0,1,0), radius=ZERO_I_OFFSET - 1, thickness=1, color = color.yellow)
 
   label_speed = label(pos = (200,-700,0), xoffset=1, box=False, opacity=0.1)
-  label_errors = label(pos = (0,-1000,0), xoffset=1, text="errors: 0", visible = False, box=False)
+  label_angle = label(pos = (-700,700,0), xoffset=1, box=False, opacity=0.1)
+  label_dist = label(pos = (-700,500,0), xoffset=1, box=False, opacity=0.1)
 
 vis = Visualization()
 
-def update_view(angle, data):
-  angle_rad = angle * math.pi / 180.0
-  c = math.cos(angle_rad)
-  s = math.sin(angle_rad)
+def update_view(start_angle, packet_data):
+  angle = start_angle
+  for data in packet_data:
 
-  dist_mm = data & 0x3fff # distance is coded on 14 bits
-  quality = 0 # quality is on 16 bits
-  #lidarData[angle] = dist_mm
-  dist_x = dist_mm*c
-  dist_y = dist_mm*s
-  qual_x = (quality + ZERO_I_OFFSET) * c
-  qual_y = (quality + ZERO_I_OFFSET) * s
+    DIST_MASK = 0x3fff
+    BAD_MASK = 0x4000 
+    FAILED_MASK = 0x8000 
 
-  #reset the point display
-  vis.point.pos[angle] = vector( 0, 0, 0 )
-  vis.pointb.pos[angle] = vector( 0, 0, 0 )
-  vis.pointbb.pos[angle] = vector( 0, 0, 0 )
-  vis.pointi.pos[angle] = vector( 0, 0, 0 )
-  vis.pointib.pos[angle] = vector( 0, 0, 0 )
-  vis.pointibb.pos[angle] = vector( 0, 0, 0 )
-  
-  FAILED_MASK = 0x8000 
-  BAD_MASK = 0x4000 
-  if data & FAILED_MASK: 
-    #vis.pointbb.pos[angle] = vector( dist_x, dist_y,0)
-    vis.pointibb.pos[angle] = vector( qual_x, qual_y, 0)
+    dist_mm = data & DIST_MASK
+    s, c = sincos[angle]
+    dist_x = dist_mm * c
+    dist_y = dist_mm * s
+    lidar_data[angle] = (dist_x, dist_y)
 
-  elif data & BAD_MASK: # quality is not as good as expected
-    vis.pointb.pos[angle] = vector( dist_x, dist_y, 0)
-    vis.pointib.pos[angle] = vector( qual_x, qual_y, 0)
-
-  else: # quality is OK
     vis.point.pos[angle] = vector( dist_x, dist_y,0)
-    vis.pointi.pos[angle] = vector( qual_x, qual_y, 0)
+    vis.pointi.pos[angle] = vector( ZERO_I_OFFSET * c, ZERO_I_OFFSET * s, 0)
+    
+    if data & FAILED_MASK or dist_mm < ZERO_I_OFFSET: 
+      vis.point.color[angle] = vector( 0, 0, 0)
+      vis.pointi.color[angle] = vector( 0.4, 0, 0)
 
+    elif data & BAD_MASK: # quality is not as good as expected
+      vis.point.color[angle] = vector( 0, 0.4, 0)
+      vis.pointi.color[angle] = vector( 0.8, 0.8, 0)
+
+    else: # quality is OK
+      vis.point.color[angle] = vector( 0, 1, 0)
+      vis.pointi.color[angle] = vector( 0, 1, 0)
+
+    angle += 1
+    if angle >= 360:
+      angle = 0
+      rel_angle, d_normal = get_relative_pos_to_wall(90)
+      vis.label_angle.text = str(rel_angle) + ' degrees'
+      vis.label_dist.text = str(d_normal) + 'mm'
+
+def get_relative_pos_to_wall(angle):
+  x1, y1 = lidar_data[angle - 5]
+  x2, y2 = lidar_data[angle + 5]
+  rel_angle_5 = math.degrees(math.atan2(y1 - y2, x1 - x2))
+  x1, y1 = lidar_data[angle - 10]
+  x2, y2 = lidar_data[angle + 10]
+  rel_angle_10 = math.degrees(math.atan2(y1 - y2, x1 - x2))
+  x1, y1 = lidar_data[angle - 15]
+  x2, y2 = lidar_data[angle + 15]
+  rel_angle_15 = math.degrees(math.atan2(y1 - y2, x1 - x2))
+
+  if abs(rel_angle_5 - rel_angle_10) > 5 or abs(rel_angle_10 - rel_angle_15) > 5:
+    rel_angle = int(min(rel_angle_5, rel_angle_10, rel_angle_15, key=lambda x: abs(x)))
+  else:
+    rel_angle = int(rel_angle_15)
+  x3, y3 = lidar_data[angle + rel_angle]
+  d_normal = (x3 ** 2 + y3 ** 2) ** 0.5
+  
+  return (-rel_angle, d_normal)
 
 def read_Lidar():
   while True:
@@ -84,20 +90,18 @@ def read_Lidar():
       vis.label_speed.text = "RPM : " + str(lidar_rpm)
       count = header[2]
 
-      data_b = ser.read(count)
-      data = struct.unpack('<' + str(count / 2) + 'H', data_b)
+      data = struct.unpack('<' + str(count / 2) + 'H', ser.read(count))
       
-      for d in data:
-        update_view(angle, d)
-        angle = angle + 1 if angle + 1 < 360 else 0
+      update_view(angle, data)
             
     except :
         traceback.print_exc(file=sys.stdout)
 
 
 if __name__ == '__main__':
-  vis = Visualization
+  com_port = "COM5"
+  baudrate = 250000
   ser = serial.Serial(com_port, baudrate)
   th = thread.start_new_thread(read_Lidar, ())
   while True:
-    rate(60) # synchonous repaint at 60fps
+    rate(24)
