cb71a75784b4910ada48303e2dd0048059be37e2
Boris Teodorovich <boristeodorovich@gmail.com>
2020-01-17 22:20:56 -0800

    Some cleanup, but overall functionality is the same

--- xv11_arduino/xv11_arduino.ino
+++ xv11_arduino/xv11_arduino.ino
@@ -3,6 +3,10 @@
   Author: Boris Teodorovch
 */
 
+// Configuration
+const uint16_t SAMPLES_CT = 60;
+const bool USE_CHECKSUM = false;
+
 uint16_t checksum(void *buf, size_t size) {
   uint32_t chk32 = 0;
   for (size_t i = 0; i < size / 2; i++) {
@@ -21,15 +25,21 @@ struct in_packet {
   uint8_t index; // 4 x 1 degree samples per packet:  0 <= (index - 0xa0) < (360 / 4) 
   uint16_t rpm;  // real rpm is actually rpm / 64
 
-  uint16_t s0_d; // the distances are in millimeters and have a 14 bit resolution
-                 // mask 0x8000 - bad data
-                 // mask 0x4000 - mediocre data
+  uint16_t s0_d: 14; // the distances are in millimeters and have a 14 bit resolution
+  bool s0_bad: 1; // mask 0x8000 - bad certainty
+  bool s0_failed: 1; // mask 0x4000 - failed measurement
   uint16_t s0_q;
-  uint16_t s1_d;
+  uint16_t s1_d: 14;
+  bool s1_bad: 1;
+  bool s1_failed: 1;
   uint16_t s1_q;
-  uint16_t s2_d;
+  uint16_t s2_d: 14;
+  bool s2_bad: 1;
+  bool s2_failed: 1;
   uint16_t s2_q;
-  uint16_t s3_d;
+  uint16_t s3_d: 14;
+  bool s3_bad: 1;
+  bool s3_failed: 1;
   uint16_t s3_q;
 
   uint16_t checksum;
@@ -56,7 +66,6 @@ void loop() {
   static size_t in_packet_bytes_read = 0;
   static unsigned int in_packet_start_index = 0;
 
-  const uint16_t SAMPLES_CT = 60;
   static out_packet<SAMPLES_CT> head;
   static unsigned int out_packet_start_index = 0;
   static size_t full_rots_offset = 0;
@@ -95,7 +104,8 @@ void loop() {
     in_packet_bytes_read += Serial2.readBytes(((uint8_t *) &p) + in_packet_bytes_read, sizeof(p) - in_packet_bytes_read);
     if (in_packet_bytes_read < sizeof(p)) { break; }
   
-    if (checksum(&p, sizeof(p) - sizeof(p.checksum)) == p.checksum) {
+    if (checksum(&p, sizeof(p) - sizeof(p.checksum)) == p.checksum || !USE_CHECKSUM) {
+      // Data valid; use
       size_t data_index = full_rots_offset + in_packet_start_index - out_packet_start_index; 
       if (data_index >= SAMPLES_CT) {
         // new in_packet won't fit in out_packet, send it and prepare for next
@@ -105,10 +115,10 @@ void loop() {
         full_rots_offset = 0;
         out_packet_start_index = in_packet_start_index;
       }
-      head.samples[data_index + 0] = p.s0_d;
-      head.samples[data_index + 1] = p.s1_d;
-      head.samples[data_index + 2] = p.s2_d;
-      head.samples[data_index + 3] = p.s3_d;
+      head.samples[data_index + 0] = p.s0_d | p.s0_bad << 14 | p.s0_failed << 15;
+      head.samples[data_index + 1] = p.s1_d | p.s1_bad << 14 | p.s1_failed << 15;
+      head.samples[data_index + 2] = p.s2_d | p.s2_bad << 14 | p.s2_failed << 15;
+      head.samples[data_index + 3] = p.s3_d | p.s3_bad << 14 | p.s3_failed << 15;
     }
     state = idle;
     break;
